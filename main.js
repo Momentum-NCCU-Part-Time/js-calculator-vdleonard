9655c722d569cd4affd333c832370faf31a89227
vanessa <NovaDReaper@yahoo.com>
2023-10-19 16:31:35 -0400

    changed display text to work better with operation Added  basic math added equals that will solove to fix still -- Text box small when no text -- can not conenule math after equals -- prettier CSS

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ let display = document.querySelectorAll("#display");
 
 let displayTextHolder = " ";
 
-
 console.log(buttons[0].classList);
 
 for (let button of buttons) {
@@ -16,17 +15,54 @@ for (let button of buttons) {
 
     let displayText = document.createElement("p");
     displayText.innerText = buttonClick.target.textContent;
+
+
+
+    if (buttonClick.target.classList.contains("button-number")){
+      console.log("Number! " + buttonClick.target.textContent)
+      displayTextHolder = displayTextHolder += displayText.innerText;
+      document.getElementById("display").textContent = displayTextHolder;
+      console.log("display text:" + displayText.innerText);
+      return;
+    } 
+
+
+    if (buttonClick.target.classList.contains("button-operation")){
+      console.log("Opperator! " + buttonClick.target.textContent)
+      displayTextHolder = displayTextHolder += displayText.innerText;
+      document.getElementById("display").textContent = displayTextHolder;
+      console.log("display text:" + displayText.innerText);
+      return;
+    } 
+
     
     if (buttonClick.target.textContent === "AC"){
-      displayTextHolder = "0";
-      
+      displayTextHolder = "";
+      document.getElementById("display").textContent = displayTextHolder;
+      console.log("display text:" + displayText.innerText);
+      return;
     
-    } else 
+    } 
+
+    if (buttonClick.target.textContent === "="){
+      console.log(" = " + buttonClick.target.textContent)
+      console.log(eval(displayTextHolder));
+      let result = eval(displayTextHolder);
+      document.getElementById("display").textContent = result;
+      displayTextHolder = "";
+      return;
     
-    displayTextHolder = displayTextHolder += displayText.innerText;
-    document.getElementById("display").textContent = displayTextHolder;
-    console.log("display text:" + displayText.innerText);
+    } 
+    
+   
+    
     
   });
+
+
+
+
+
+
 }
 
